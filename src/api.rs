b47dda8acd382919c5be621f08692fc12cd36112
Nikita Stefaniak <phaux4@gmail.com>
2021-12-21 12:18:04 +0100

    organize code

---
+++ src/api.rs
@@ -0,0 +1,136 @@
+use std::{collections::HashMap, convert::Infallible, sync::Arc};
+
+use acdc::{Attestation, Hashed, Signed};
+use openssl::{
+    pkey::{PKey, Private},
+    sign::Signer,
+};
+use tokio::sync::RwLock;
+use warp::Filter;
+
+#[derive(Debug)]
+enum ApiError {
+    SigningError,
+    InvalidAttestation,
+}
+
+impl warp::Reply for ApiError {
+    fn into_response(self) -> warp::reply::Response {
+        let mut resp = warp::reply::Response::new(format!("{:?}", self).into());
+        *resp.status_mut() = warp::hyper::StatusCode::INTERNAL_SERVER_ERROR;
+        resp
+    }
+}
+
+impl warp::reject::Reject for ApiError {}
+
+pub(crate) type AttestationDB = Arc<RwLock<HashMap<String, Signed<Hashed<Attestation>>>>>;
+
+pub(crate) type KeyDB = Arc<RwLock<HashMap<String, acdc::PubKey>>>;
+
+pub(crate) fn setup_routes(
+    attest_db: AttestationDB,
+    pub_keys: KeyDB,
+    priv_key: Arc<RwLock<PKey<Private>>>,
+) -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let attest_list_route = warp::path("attestations")
+        .and(warp::get())
+        .and(warp::any().map({
+            let attest_db = attest_db.clone();
+            move || attest_db.clone()
+        }))
+        .then(attest_list)
+        .map(handle_result);
+
+    let attest_create_route = warp::path("attestations")
+        .and(warp::path("create"))
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(warp::any().map({
+            let attest_db = attest_db.clone();
+            move || attest_db.clone()
+        }))
+        .and(warp::any().map({
+            let priv_key = priv_key;
+            move || priv_key.clone()
+        }))
+        .then(attest_create)
+        .map(handle_result);
+
+    let attest_receive_route = warp::path("attestations")
+        .and(warp::post())
+        .and(warp::body::bytes())
+        .and(warp::any().map({
+            let attest_db = attest_db;
+            move || attest_db.clone()
+        }))
+        .and(warp::any().map({
+            let pub_keys = pub_keys;
+            move || pub_keys.clone()
+        }))
+        .then(attest_receive)
+        .map(handle_result);
+
+    attest_list_route
+        .or(attest_create_route)
+        .or(attest_receive_route)
+}
+
+fn handle_result(result: Result<impl warp::Reply, impl warp::Reply>) -> impl warp::Reply {
+    match result {
+        Ok(val) => val.into_response(),
+        Err(err) => err.into_response(),
+    }
+}
+
+async fn attest_list(attest_db: AttestationDB) -> Result<warp::reply::Json, Infallible> {
+    let attest_db = attest_db.read().await;
+    let attests = attest_db
+        .iter()
+        .map(|(_id, attest)| &attest.data)
+        .collect::<Vec<_>>();
+    Ok(warp::reply::json(&attests))
+}
+
+async fn attest_create(
+    attest: Attestation,
+    attest_db: AttestationDB,
+    priv_key: Arc<RwLock<PKey<Private>>>,
+) -> Result<warp::reply::Json, ApiError> {
+    let attest = Hashed::new(attest);
+    let sig = {
+        let priv_key = &*priv_key.read().await;
+        let mut signer =
+            Signer::new_without_digest(priv_key).map_err(|_| (ApiError::SigningError))?;
+        signer
+            .sign_oneshot_to_vec(&Signed::get_json_bytes(&attest))
+            .map_err(|_| (ApiError::SigningError))?
+    };
+    let attest = Signed::new_with_ed25519(attest, &sig).map_err(|_| (ApiError::SigningError))?;
+    {
+        let mut attest_db = attest_db.write().await;
+        attest_db.insert(attest.data.get_hash().to_string(), attest.clone());
+    }
+    Ok(warp::reply::json(&attest.data))
+}
+
+async fn attest_receive(
+    attest: warp::hyper::body::Bytes,
+    attest_db: AttestationDB,
+    pub_keys: KeyDB,
+) -> Result<warp::reply::Json, ApiError> {
+    let attest = std::str::from_utf8(&attest).map_err(|_| ApiError::InvalidAttestation)?;
+    let attest = Signed::<Hashed<Attestation>>::from_signed_json(attest)
+        .map_err(|_| ApiError::InvalidAttestation)?;
+    {
+        let pub_keys = pub_keys.read().await;
+        attest
+            .verify(&pub_keys)
+            .map_err(|_| ApiError::InvalidAttestation)?;
+    }
+    {
+        let mut attest_db = attest_db.write().await;
+        attest_db.insert(attest.data.get_hash().to_string(), attest.clone());
+    }
+    Ok(warp::reply::json(&attest.data))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
+mod api;
+
 use std::{
     collections::HashMap,
-    convert::Infallible,
     path::{Path, PathBuf},
     sync::Arc,
 };
 
-use acdc::{Attestation, Hashed, Signed};
 use anyhow::Context;
-use openssl::{
-    pkey::{PKey, Private},
-    sign::Signer,
-};
+use openssl::pkey::{PKey, Private};
 use structopt::StructOpt;
 use tokio::{
     fs::File,
     io::{AsyncReadExt, AsyncWriteExt},
     sync::RwLock,
 };
-use warp::{
-    hyper::{body::Bytes, StatusCode},
-    reply::Response,
-    Filter, Reply,
-};
+
+use self::api::{setup_routes, AttestationDB};
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -40,26 +34,6 @@ struct Opts {
     port: u16,
 }
 
-#[derive(Debug)]
-enum ApiError {
-    SigningError,
-    InvalidAttestation,
-}
-
-impl Reply for ApiError {
-    fn into_response(self) -> Response {
-        let mut resp = Response::new(format!("{:?}", self).into());
-        *resp.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-        resp
-    }
-}
-
-impl warp::reject::Reject for ApiError {}
-
-type AttestationDB = Arc<RwLock<HashMap<String, Signed<Hashed<Attestation>>>>>;
-
-type KeyDB = Arc<RwLock<HashMap<String, acdc::PubKey>>>;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let Opts {
@@ -68,107 +42,15 @@ async fn main() -> anyhow::Result<()> {
         port,
     } = Opts::from_args();
 
-    let priv_key = load_priv_key(&priv_key_path).await?;
+    let priv_key = Arc::new(RwLock::new(load_priv_key(&priv_key_path).await?));
     let pub_keys = Arc::new(RwLock::new(load_pub_keys(&pub_keys_path).await?));
     let attest_db: AttestationDB = Arc::new(RwLock::new(HashMap::new()));
 
-    let attest_list_route = warp::path("attestations")
-        .and(warp::get())
-        .and(warp::any().map({
-            let attest_db = attest_db.clone();
-            move || attest_db.clone()
-        }))
-        .then(attest_list)
-        .map(handle_result);
-
-    let attest_create_route = warp::path("attestations")
-        .and(warp::path("create"))
-        .and(warp::post())
-        .and(warp::body::json())
-        .and(warp::any().map({
-            let attest_db = attest_db.clone();
-            move || attest_db.clone()
-        }))
-        .and(warp::any().map(move || priv_key.clone()))
-        .then(attest_create)
-        .map(handle_result);
-
-    let attest_receive_route = warp::path("attestations")
-        .and(warp::post())
-        .and(warp::body::bytes())
-        .and(warp::any().map({
-            let attest_db = attest_db.clone();
-            move || attest_db.clone()
-        }))
-        .and(warp::any().map({
-            let pub_keys = pub_keys.clone();
-            move || pub_keys.clone()
-        }))
-        .then(attest_receive)
-        .map(handle_result);
-
-    let routes = attest_list_route
-        .or(attest_create_route)
-        .or(attest_receive_route);
+    let routes = setup_routes(attest_db, pub_keys, priv_key);
 
     warp::serve(routes).run(([127, 0, 0, 1], port)).await;
 
     Ok(())
-}
-
-fn handle_result(result: Result<impl Reply, impl Reply>) -> impl Reply {
-    match result {
-        Ok(val) => val.into_response(),
-        Err(err) => err.into_response(),
-    }
-}
-
-async fn attest_list(attest_db: AttestationDB) -> Result<warp::reply::Json, Infallible> {
-    let attest_db = attest_db.read().await;
-    let attests = attest_db
-        .iter()
-        .map(|(_id, attest)| &attest.data)
-        .collect::<Vec<_>>();
-    Ok(warp::reply::json(&attests))
-}
-
-async fn attest_create(
-    attest: Attestation,
-    attest_db: AttestationDB,
-    key: PKey<Private>,
-) -> Result<warp::reply::Json, ApiError> {
-    let attest = Hashed::new(attest);
-    let mut signer = Signer::new_without_digest(&key).map_err(|_| (ApiError::SigningError))?;
-    let sig = signer
-        .sign_oneshot_to_vec(&Signed::get_json_bytes(&attest))
-        .map_err(|_| (ApiError::SigningError))?;
-    let attest = Signed::new_with_ed25519(attest, &sig).map_err(|_| (ApiError::SigningError))?;
-    {
-        let mut attest_db = attest_db.write().await;
-        attest_db.insert(attest.data.get_hash().to_string(), attest.clone());
-    }
-    Ok(warp::reply::json(&attest.data))
-}
-
-async fn attest_receive(
-    attest: Bytes,
-    attest_db: AttestationDB,
-    pub_keys: KeyDB,
-) -> Result<warp::reply::Json, ApiError> {
-    let attest = std::str::from_utf8(&attest).map_err(|_| ApiError::InvalidAttestation)?;
-    let attest = Signed::<Hashed<Attestation>>::from_signed_json(attest)
-        .map_err(|_| ApiError::InvalidAttestation)?;
-    {
-        let pub_keys = pub_keys.read().await;
-        attest
-            .verify(&pub_keys)
-            .map_err(|_| ApiError::InvalidAttestation)?;
-    }
-    {
-        let mut attest_db = attest_db.write().await;
-        attest_db.insert(attest.data.get_hash().to_string(), attest.clone());
-    }
-    Ok(warp::reply::json(&attest.data))
 }
 
 async fn load_priv_key(path: &Path) -> anyhow::Result<PKey<Private>> {
